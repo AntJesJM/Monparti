e56efbc560535b83b2572efa495ab78dc011c572
JSureda <jesussureda@gmail.com>
2018-01-31 11:44:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jsureda/monparti/Bienvenida.java
@@ -0,0 +1,13 @@
+package com.example.jsureda.monparti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bienvenida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+    }
+}
