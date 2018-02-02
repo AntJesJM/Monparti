c1ed982dfc91d8ac9a6b84cfba85ca82e3969799
Josemi Gonzalez <xemy.013@gmail.com>
2018-02-03 00:03:46 +0100

    creacion del metodo para la ventana de bienvenida

--- app/src/main/java/com/example/jsureda/monparti/Bienvenida.java
+++ app/src/main/java/com/example/jsureda/monparti/Bienvenida.java
@@ -1,13 +1,23 @@
 package com.example.jsureda.monparti;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Bienvenida extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(Bienvenida.this, AgregarEntrada.class);
+                startActivity(homeIntent);
+            }
+        }.SPLASH_TIME_OUT);
     }
 }
