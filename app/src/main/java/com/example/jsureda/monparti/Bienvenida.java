af1739abeb58a3144cb0564067a0dd0f09b7a63a
Josemi Gonzalez <xemy.013@gmail.com>
2018-02-05 12:19:58 +0100

    Correcto funcionamiento de la pantalla bienvenida a falta de retoques visuales

--- app/src/main/java/com/example/jsureda/monparti/Bienvenida.java
+++ app/src/main/java/com/example/jsureda/monparti/Bienvenida.java
@@ -1,15 +1,25 @@
 package com.example.jsureda.monparti;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Bienvenida extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+//Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_bienvenida);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
         new Handler().postDelayed(new Runnable() {
@@ -17,7 +27,8 @@ public class Bienvenida extends AppCompatActivity {
             public void run() {
                 Intent homeIntent = new Intent(Bienvenida.this, AgregarEntrada.class);
                 startActivity(homeIntent);
+                finish();
             }
-        }.SPLASH_TIME_OUT);
+        }, SPLASH_TIME_OUT);
     }
 }
