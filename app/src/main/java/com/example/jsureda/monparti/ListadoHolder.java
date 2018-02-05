874acdd6fd3ea47ea8a23074c390ff9d80dc259c
UNO <a.baena@andaluciajunta.es>
2018-02-05 12:21:24 +0100

    Se ha creado la activity de la lista de lugares y el holder

---
+++ app/src/main/java/com/example/jsureda/monparti/ListadoHolder.java
@@ -0,0 +1,13 @@
+package com.example.jsureda.monparti;
+
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class ListadoHolder {
+    TextView nombre;
+    RatingBar nota;
+    ImageView foto;
+
+
+}
