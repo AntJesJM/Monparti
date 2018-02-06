b25c5867bf6e067ccce8ceaf46f09f0170f79382
Josemi Gonzalez <xemy.013@gmail.com>
2018-02-06 21:00:39 +0100

    Cambios en el fichero de colores basicos para la aplicacion y mejora de la pantalla de bienvenida

--- app/src/main/java/com/example/jsureda/monparti/Bienvenida.java
+++ app/src/main/java/com/example/jsureda/monparti/Bienvenida.java
@@ -1,14 +1,14 @@
 package com.example.jsureda.monparti;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Bienvenida extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1000;
+public class Bienvenida extends Activity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
