a93c6a3da934c1e124e50977e2ee690d194b71ab
JSureda <jesussureda@gmail.com>
2018-02-21 10:19:16 +0100

    Se ha añadido funcionalidad al botón de la cámara y se han solucionado problemas al desplegar el Time Picker

--- app/src/main/java/com/example/jsureda/monparti/IntroducirLugar.java
+++ app/src/main/java/com/example/jsureda/monparti/IntroducirLugar.java
@@ -2,7 +2,11 @@ package com.example.jsureda.monparti;
 
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +15,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import com.google.android.gms.maps.MapView;
 
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
 import java.util.Calendar;
 
 public class IntroducirLugar extends AppCompatActivity {
@@ -26,6 +33,8 @@ public class IntroducirLugar extends AppCompatActivity {
     MapView mapa;
     ImageButton guardar, galeria, camara;
     ImageView imagen;
+    private static int DESDE_CAMARA = 1;
+    private static int DESDE_GALERIA = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,6 +95,27 @@ public class IntroducirLugar extends AppCompatActivity {
 
             }
         });
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = null;
+                int code = 0;
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                code = DESDE_CAMARA;
+                startActivityForResult(intent, code);
+            }
+        });
+
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = null;
+                int code = 0;
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                code = DESDE_GALERIA;
+            }
+        });
     }
 
     public void inicializarUI() {
@@ -98,8 +128,28 @@ public class IntroducirLugar extends AppCompatActivity {
         guardar = (ImageButton)findViewById(R.id.iBtnGuardar);
         galeria = (ImageButton)findViewById(R.id.iBtnGaleria);
         camara = (ImageButton)findViewById(R.id.iBtnCamara);
-        imagen = (ImageView)findViewById(R.id.imgAned);
 
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Bitmap imagen = null;
+
+        if (requestCode == DESDE_CAMARA) {
+            imagen = (Bitmap) data.getParcelableExtra("data");
+        }
+
+        if (requestCode == DESDE_GALERIA) {
+            Uri rutaImagen = data.getData();
+            try {
+                imagen = BitmapFactory.decodeStream(new BufferedInputStream(getContentResolver().openInputStream(rutaImagen)));
+            } catch (FileNotFoundException e) { }
+        }
+
+        ImageView iv = (ImageView) findViewById(R.id.imgAned);
+        iv.setImageBitmap(imagen);
     }
 
 }
