8753ddeab20dbfa9da48b693ee8ac42dd0cdbbf6
JSureda <jesussureda@gmail.com>
2018-02-26 20:48:35 +0100

    Ventana Introducir Lugar: Cambios en el funcionamiento del mapa, introducción de permisos.

--- app/src/main/java/com/example/jsureda/monparti/IntroducirLugar.java
+++ app/src/main/java/com/example/jsureda/monparti/IntroducirLugar.java
@@ -1,20 +1,19 @@
 package com.example.jsureda.monparti;
 
+import android.Manifest;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -25,46 +24,39 @@ import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
-import com.google.android.gms.maps.CameraUpdateFactory;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.BufferedInputStream;
 import java.io.FileNotFoundException;
 import java.util.Calendar;
 
-public class IntroducirLugar extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+public class IntroducirLugar extends AppCompatActivity implements OnMapReadyCallback {
     EditText nombre, descripcion, apertura, cierre;
     Spinner categorias;
     RatingBar barra;
     ImageButton guardar, galeria, camara;
     private static int DESDE_CAMARA = 1;
     private static int DESDE_GALERIA = 2;
+    GoogleMap mGoogleMap;
     SupportMapFragment mapFragment;
-    GoogleMap mMap;
-    double latitude ;
-    double longitude ;
-    Criteria criteria ;
-    String bestProvider;
-    Location location ;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 80;
+    private static final int MY_PERMISSIONS_REQUEST_GALLERY = 90;
+    private static final int MY_PERMISSIONS_CAMERA = 100;
+    Location loc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introducir_lugar);
         inicializarUI();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(IntroducirLugar.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(IntroducirLugar.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }           locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, (LocationListener) IntroducirLugar.this);
-        criteria = new Criteria();
-        bestProvider = locationManager.getBestProvider(criteria, true);
-        location = locationManager.getLastKnownLocation(bestProvider);
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapViewAned);
         mapFragment.getMapAsync(this);
@@ -76,12 +68,12 @@ public class IntroducirLugar extends AppCompatActivity implements OnMapReadyCall
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 Intent homeIntent = new Intent(IntroducirLugar.this, Listado.class);
+
                 startActivity(homeIntent);
                 finish();
             }
         });
 
-        // perform click event listener on edit text
         apertura.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,7 +93,6 @@ public class IntroducirLugar extends AppCompatActivity implements OnMapReadyCall
             }
         });
 
-        // perform click event listener on edit text
         cierre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -123,55 +114,49 @@ public class IntroducirLugar extends AppCompatActivity implements OnMapReadyCall
         camara.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = null;
-                int code = 0;
-                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                code = DESDE_CAMARA;
-                startActivityForResult(intent, code);
+                askForPermission(Manifest.permission.CAMERA, MY_PERMISSIONS_CAMERA);
+                if (ContextCompat.checkSelfPermission(IntroducirLugar.this,
+                        Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = null;
+                    int code = 0;
+                    intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    code = DESDE_CAMARA;
+                    startActivityForResult(intent, code);
+                } else {
+                    return;
+                }
             }
         });
 
         galeria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = null;
-                int code = 0;
-                intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                code = DESDE_GALERIA;
-                startActivityForResult(intent, code);
+                askForPermission(Manifest.permission.READ_EXTERNAL_STORAGE, MY_PERMISSIONS_REQUEST_GALLERY);
+                if (ContextCompat.checkSelfPermission(IntroducirLugar.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent intent = null;
+                    int code = 0;
+                    intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    code = DESDE_GALERIA;
+                    startActivityForResult(intent, code);
+                } else {
+                    return;
+                }
             }
         });
 
         guardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (location == null) {
-                    Toast.makeText(getApplicationContext(), "GPS signal not found", Toast.LENGTH_SHORT).show();
-                }
-                if (location != null) {
-                    latitude = location.getLatitude();
-                    longitude = location.getLongitude();
-                    LatLng posicion = new LatLng(latitude, longitude);
-                    mMap.addMarker(new MarkerOptions().position(posicion)
-                            .title("Posición"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(posicion));
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                    onLocationChanged(location);
+                loc = mGoogleMap.getMyLocation();
+                if (loc != null) {
+                    Toast.makeText(IntroducirLugar.this, "Long: " + loc.getLongitude() + ", Lat: " + loc.getLatitude(), Toast.LENGTH_LONG).show();
                 }
             }
         });
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        mMap = map;
-        mMap.setMinZoomPreference(15.0f);
-        mMap.setMaxZoomPreference(20.0f);
-        LatLng posicionInicial = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(posicionInicial)
-                .title("Posición"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(posicionInicial));
     }
 
     public void inicializarUI() {
@@ -184,7 +169,6 @@ public class IntroducirLugar extends AppCompatActivity implements OnMapReadyCall
         guardar = (ImageButton) findViewById(R.id.iBtnGuardar);
         galeria = (ImageButton) findViewById(R.id.iBtnGaleria);
         camara = (ImageButton) findViewById(R.id.iBtnCamara);
-
     }
 
     @Override
@@ -204,28 +188,65 @@ public class IntroducirLugar extends AppCompatActivity implements OnMapReadyCall
         }
         ImageView iv = (ImageView) findViewById(R.id.imgAned);
         iv.setImageBitmap(imagen);
-
-
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    mGoogleMap.setMyLocationEnabled(true);
+                } else {
+                }
+            }
+
+            case MY_PERMISSIONS_REQUEST_GALLERY: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+                } else {
+                }
+            }
+            case MY_PERMISSIONS_CAMERA: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+                } else {
+                }
+            }
+        }
+    }
+
+    private void askForPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(IntroducirLugar.this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(IntroducirLugar.this, permission)) {
+                ActivityCompat.requestPermissions(IntroducirLugar.this, new String[]{permission}, requestCode);
+
+            } else {
+                ActivityCompat.requestPermissions(IntroducirLugar.this, new String[]{permission}, requestCode);
+            }
+        } else {
+            //Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
-    public void onLocationChanged(Location location) {
+    public void onMapReady(GoogleMap googleMap) {
+        askForPermission(Manifest.permission.ACCESS_FINE_LOCATION, MY_PERMISSIONS_REQUEST_LOCATION);
+        mGoogleMap = googleMap;
+        loc = mGoogleMap.getMyLocation();
     }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-    }
 }
